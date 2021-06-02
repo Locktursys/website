7a0b352212754e05a469df09280482747cf220a6
danielkormos1993 <danielkormos1993@gmail.com>
2021-06-02 09:32:44 +0200

    new version ready see todos

---
+++ build.js
@@ -0,0 +1,17 @@
+﻿const fs = require("fs");
+const ejsRenderFile = require("ejs").renderFile;
+const { src, dest } = require("gulp");
+const concat = require("gulp-concat");
+const cleancss = require("gulp-clean-css");
+const sort = require('gulp-sort');
+
+ejsRenderFile(`src/index.ejs`)
+.then(html => fs.writeFileSync(`index.html`, html))
+.catch(err => console.error(err));
+
+// CSS GENERATOR
+src("src/**/*.css")
+.pipe(sort())
+.pipe(concat("index.css"))
+.pipe(cleancss())
+.pipe(dest("./"));
